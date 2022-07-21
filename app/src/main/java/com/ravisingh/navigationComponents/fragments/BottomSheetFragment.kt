1ba71050187f9d6be8cf097381e7a9b884b2938e
Ravi Singh <rpoolapps@gmail.com>
2022-07-21 23:52:59 +0530

    New Bottom Sheet added

--- app/src/main/java/com/ravisingh/navigationComponents/fragments/BottomSheetFragment.kt
+++ app/src/main/java/com/ravisingh/navigationComponents/fragments/BottomSheetFragment.kt
@@ -5,14 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.ravisingh.navigationComponents.R
-import kotlinx.android.synthetic.main.fragment_fifth.view.*
 
-class FifthFragment : Fragment() {
+class BottomSheetFragment : BottomSheetDialogFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
@@ -20,14 +24,11 @@ class FifthFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fifth, container, false)
+        return inflater.inflate(R.layout.fragment_bottom_sheet, container, false)
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        view.show_bottom_sheet.setOnClickListener{
-            findNavController().navigate(R.id.action_fifthFragment_to_bottomSheetFragment)
-        }
     }
 }
